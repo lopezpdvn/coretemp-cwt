5ed340ece6682ac3619e5a0b349c2ed259231102
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-05-12 15:36:20 -0700

    Update analyze.m

--- analyze.m
+++ analyze.m
@@ -1,22 +1,20 @@
 
 tz = "America/Los_Angeles"
 temps_file = '~/Downloads/core_export.csv'
-raw_temps = readtable(temps_file, setvartype(detectImportOptions(temps_file), {'Var1'}, 'uint64'))
+raw_temps = readtable(temps_file, setvartype(detectImportOptions(temps_file), {'heat_strain_index', 'heart_rate_bpm_'}, 'double'))
 
 period_range_start = hours(24-1.5)
 period_range_end = hours(24+1.5)
 
 
 raw_temps_with_datetimes = raw_temps
-raw_temps_with_datetimes.Var1 = arrayfun(@(x) datetime(x, ConvertFrom='epochtime', TicksPerSecond=1000, TimeZone='UTC'), raw_temps_with_datetimes.Var1)
-raw_temps_with_datetimes.Var1.TimeZone = tz
 
 irregular_temps = table2timetable(raw_temps_with_datetimes)
 
 temps = retime(irregular_temps, 'regular', 'linear', TimeStep=minutes(5))
 
-[wt, period, coi] = cwt(temps.Var2, 'amor', hours(5/60), VoicesPerOctave=48, ExtendSignal=false)
-xrec = icwt(wt, period, [period_range_start, period_range_end], SignalMean=mean(temps.Var2))
+[wt, period, coi] = cwt(temps.core_temperature_F_, 'amor', hours(5/60), VoicesPerOctave=48, ExtendSignal=false)
+xrec = icwt(wt, [], period, [period_range_start, period_range_end], SignalMean=mean(temps.core_temperature_F_))
 
 
 halflen = floor(length(coi)/2)
@@ -27,7 +25,7 @@ coi_end_index = coi_end_index + halflen
 temps_trimmed = temps(coi_start_index:coi_end_index, :)
 xrec_trimmed = xrec(:, coi_start_index:coi_end_index)
 
-%cwt(temps.Var2, 'amor', hours(5/60), VoicesPerOctave=48)
+%cwt(temps.core_temperature_F_, 'amor', hours(5/60), VoicesPerOctave=48)
 
 [pks, maxlocs] = findpeaks(xrec_trimmed)
 [pks, minlocs] = findpeaks(-xrec_trimmed)
@@ -35,17 +33,17 @@ diff(maxlocs)*minutes(5) - minutes(1440)
 diff(minlocs)*minutes(5) - minutes(1440)
 
 % print out circadian nadir times
-temps_trimmed.Var1(minlocs)
+temps_trimmed.date_time_local(minlocs)
 
-plot(temps.Var1, temps.Var2);
+plot(temps.date_time_local, temps.core_temperature_F_);
 
 hold on;
-%rectangle(Position=[tt_trimmed.Var1(1), 100, hours(1), 2], FaceColor=[0.9 0.9 0.9], EdgeColor='none')
-plot(temps_trimmed.Var1, xrec_trimmed, LineWidth=2);
+%rectangle(Position=[tt_trimmed.date_time_local(1), 100, hours(1), 2], FaceColor=[0.9 0.9 0.9], EdgeColor='none')
+plot(temps_trimmed.date_time_local, xrec_trimmed, LineWidth=2);
 
 % Plot vertical lines denoting area of certainty
-plot([temps.Var1(coi_start_index) temps.Var1(coi_start_index)], [min(temps_trimmed.Var2), max(temps_trimmed.Var2)])
-plot([temps.Var1(coi_end_index) temps.Var1(coi_end_index)], [min(temps_trimmed.Var2), max(temps_trimmed.Var2)])
+plot([temps.date_time_local(coi_start_index) temps.date_time_local(coi_start_index)], [min(temps_trimmed.core_temperature_F_), max(temps_trimmed.core_temperature_F_)])
+plot([temps.date_time_local(coi_end_index) temps.date_time_local(coi_end_index)], [min(temps_trimmed.core_temperature_F_), max(temps_trimmed.core_temperature_F_)])
 
 sleep_bar_height = 0.1 % degrees
 sleep_bar_y_offset = 97.5
